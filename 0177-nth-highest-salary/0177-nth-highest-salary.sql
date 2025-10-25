d73d29d9a8917aa3ed8dda76f38d4cae7c52916a
Satish naidu Vulli <satishnaidu2633@gmail.com>
2025-10-25 07:26:27 +0530

    Time: 487 ms (20.92%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      select distinct salary from Employee e1 where N-1 = (select count(distinct salary) from Employee e2 where e2.salary > e1.salary) limit 1
+
+  );
+END
